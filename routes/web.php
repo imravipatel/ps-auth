2141a702be3e4dbe8d8a864f9bd8c6af90bb5327
Ravi Patel <ravi.engineer02@gmail.com>
2022-09-08 14:19:07 +0530

    Grouping the Routes with common auth middleware

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::middleware(['auth'])->group(function (){
+    Route::get('/dashboard', function () { return view('dashboard'); })->name('dashboard');
+    Route::get('/dashboard/clients', 
+        function(Request $request){ return view('clients',[
+                'clients' => $request->user()->clients
+            ]);
+        })->name('dashboard.clients');
+});
 
-Route::get('/dashboard/clients', function(Request $request){
-    // dd($request);
-    return view('clients',[
-        'clients' => $request->user()->clients
-    ]);
-})->middleware(['auth'])->name('dashboard.clients');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
+
+// Route::get('/dashboard/clients', function(Request $request){
+//     // dd($request);
+//     return view('clients',[
+//         'clients' => $request->user()->clients
+//     ]);
+// })->middleware(['auth'])->name('dashboard.clients');
 
 require __DIR__.'/auth.php';
